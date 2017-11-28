31f80ce65f539153c0145932be427f24d3961829
aseemgirdhar <aseemgirdhar9@gmail.com>
2017-11-28 18:16:44 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    NavComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
